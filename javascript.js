5fa105807cb768b92243552e9efbba9fb335dab2
Paul Gerdes <paul-gerds@gmx.net>
2023-09-18 19:12:58 +0200

    Lois stinkt

---
+++ javascript.js
@@ -0,0 +1,12 @@
+//Aufgabe 1
+
+function test1() {
+  var hello = "test 1";
+  console.log(hello);
+  var hello = "test 2";
+  console.log(hello);
+  if (true) {
+    var hello = "test 3";
+  }
+  console.log(hello);
+}
